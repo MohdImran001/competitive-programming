4f458f87db38d96533b6b566f5e58ec1755331d8
Mohammad Imran (Code Crusher) <imrancrazy200@gmail.com>
2022-10-02 19:34:42 +0530

    feat: init repo

---
+++ template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define vi vector<int>
+#define vc vector<char>
+#define vll vector<ll>
+#define vpp vector<pair<ll, ll>>
+
+#define mp make_pair
+#define pb push_back
+#define forn(i, n) for (int i = 0; i < int(n); ++i)
+#define _forn(i, n) for (int i = int(n); i >= 0; --i)
+
+using namespace std;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+int main()
+{
+    return 0;
+}
